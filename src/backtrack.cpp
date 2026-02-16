8142dccdb761d053d17c46e271d3117c15fdef4d
manuellthimoty <manuelsilalahi06@gmail.com>
2026-02-16 16:27:52 +0700

    add flush to output all possible combn

--- src/backtrack.cpp
+++ src/backtrack.cpp
@@ -1,113 +1,124 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-// hal hal yg dibutuhkan untuk permuttasi
+
 vector<int> arr;
 vector<bool> used;
-vector<vector<int>> permutations;
-int n;
 
-// set banyak daerah yg mungkin
+int n;
+ll iterationCount = 0;
 
 vector<vector<string>> ans;
+vector<vector<string>> tempAns;
 vector<string> input;
-void generatePermutations(vector<int> curr ={}){
-    if(n == curr.size()){
-        for(int i = 0 ; i < curr.size()-1 ; i++){
-            if(abs(curr[i] - curr[i+1]) == 1){
-                return;
-            }
-        }
-        permutations.push_back(curr);
-        return;
+vector<vector<char>> regions;
+
+void generateCopy(vector<string> in, vector<int> pos){
+    vector<string> copyin;
+    for(auto s : in) copyin.push_back(s);
+    int posSize = pos.size();
+    for(int i = 0 ; i < posSize ; i++){
+        copyin[pos[i]][i] = '#';
     }
-    for(int i = 0 ; i < arr.size() ; i++){
-        if(!used[i]){
-            used[i] = true;
-            curr.push_back(arr[i]);
-            generatePermutations(curr);
-            curr.pop_back();
-            used[i] = false ;
-        }
-    }
+    tempAns.push_back(copyin);
 }
+
 
 void generateAnswer(vector<string> in, vector<int> pos){
     vector<string> copyin;
     for(auto s : in) copyin.push_back(s);
     int posSize = pos.size();
     for(int i = 0 ; i < posSize ; i++){
-        copyin[pos[i] -1][i] = '#';
+        copyin[pos[i]][i] = '#';
     }
     ans.push_back(copyin);
+    tempAns.push_back(copyin);
 }
 
-
-void solve(){
+bool check(vector<int> curr){
+    int countRegion[256] = {0};
+    for(int i = 0 ; i < curr.size() ; i++){
+        int curReg = regions[curr[i]][i];
+        countRegion[curReg] ++;
+        if(countRegion[curReg] > 1){
+            return false;
+        }
+        if(i < n-1 && abs(curr[i] - curr[i+1]) <=1) return false;
+    }
+    return true;
+}
+void outputProgress() {
     
+    if(tempAns.size() > 0) {
+        for(auto a : tempAns){
+            for(auto line : a){
+                cout << line << endl;
+            }
+            cout << endl;
+        }
+        cout.flush();
+        tempAns.clear();
+    }
 }
+
+void generateProgress(vector<int> currProgress) {
+    vector<string> progressCopy;
+    for(auto s : input) progressCopy.push_back(s);
+    int posSize = currProgress.size();
+    for(int i = 0 ; i < posSize ; i++){
+        progressCopy[currProgress[i]][i] = '#';
+    }
+    tempAns.push_back(progressCopy);
+}
+
+void solve(vector<int> curr = {}){
+    // Output intermediate results every 1000 iterations
+    
+    if(n == (int)curr.size()){
+        iterationCount++;
+
+        if(iterationCount % 100 == 0) {
+            generateProgress(curr);
+            outputProgress();
+        }
+        if(check(curr)){
+            generateAnswer(input,curr);
+        }
+        return;
+    }
+    for(int i = 0 ; i < (int) arr.size() ; i++){
+        if(!used[i]){
+            used[i] = true;
+            curr.push_back(arr[i]);
+            solve(curr);
+            curr.pop_back();
+            used[i] = false;
+        }
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     cin >> n;
-    for(int i = 1 ; i <= n ; i++){
+    for(int i = 0; i < n ; i++){
         arr.push_back(i);
         used.push_back(false);
     }
-    generatePermutations();
-    vector<vector<char>> regions(n+1, vector<char>(n+1));
-    for(int i = 1 ; i <= n ; i++){
+    regions.resize(n, vector<char>(n));
+
+    for(int i = 0 ; i < n ; i++){
         string line ; cin >> line;
         input.push_back(line);
-        for(int j = 1 ; j<= n ; j ++){
-            regions[i][j] = line[j-1];
+        for(int j = 0 ; j< n ; j ++){
+            regions[i][j] = line[j];
         }
     }
-    // for(int i = 1; i <= n; i++){
-    //     for(int j = 1; j <= n; j++){
-    //         cout << regions[i][j];
-    //     }
-    //     cout << endl;
-    // }
-    cout << permutations.size() << endl;
-    for(auto p : permutations){
-        // val dari p adalah nilai2 kolomnya.
-        bool valid = true;
-        // map<int,int> countSum;
-        // for(int i = 1; i <= n ; i++){
-        //     int curVal = i + p[i-1];
-        //     countSum[curVal] ++ ;
-        //     if(countSum[curVal] > 1){
-        //         cout << curVal;
-        //         valid = false;
-        //         break;
-        //     }
-        // }
-        // map<int,int> countDiff;
-        // for(int i = 1; i <= n; i++){
-        //     int curDiff = p[i-1]- i;
-        //     countDiff[curDiff]++;
-        //     if(countDiff[curDiff] > 1){
-        //         valid = false;
-        //         break;
-        //     }
-        // }
-        // map<char,int> countRegion;
-        int countRegion[256] = {0};
-        for(int i = 1;  i <= n ; i++){
-            int curReg = regions[p[i-1]][i];
-            countRegion[curReg] ++;
-            if(countRegion[curReg] > 1){
-                valid = false;
-                break;
-            }
-        }
-        if(valid){
-            generateAnswer(input,p);
-            break;
-        }
-    }
+
+    solve();
+    
+    // Output remaining results at the end
     for(auto a : ans){
         for(auto line : a){
             cout << line << endl;
